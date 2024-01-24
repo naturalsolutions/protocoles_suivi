c761f954c78f65e766dbeb927966de4708ab04ab
Andria Capai <andria_capai@natural-solutions.eu>
2024-01-24 16:20:52 +0100

    fix: synthese sql

--- synthese.sql
+++ synthese.sql
@@ -21,7 +21,8 @@ CREATE VIEW gn_monitoring.v_synthese_:module_code AS WITH source AS (
 	FROM gn_synthese.t_sources
 	WHERE name_source = CONCAT('MONITORING_', UPPER(:'module_code'))
 	LIMIT 1
-), sites AS (
+)
+, sites AS (
 	SELECT id_base_site,
 		geom AS the_geom_4326,
 		ST_CENTROID(geom) AS the_geom_point,
@@ -35,6 +36,7 @@ visits AS (
 		id_base_site,
 		id_dataset,
 		id_digitiser,
+		observers_txt,
 		visit_date_min AS date_min,
 		COALESCE (visit_date_max, visit_date_min) AS date_max,
 		comments,
@@ -55,7 +57,17 @@ observers AS (
 	FROM gn_monitoring.cor_visit_observer cvo
 		JOIN utilisateurs.t_roles r ON r.id_role = cvo.id_role
 	GROUP BY id_base_visit
-)
+),
+determiner AS (
+	    SELECT
+	        array_agg(r.id_role) AS id_determiner,
+	        CONCAT(r.nom_role, ' ', prenom_role) AS determiner,
+	        id_base_visit
+	    FROM gn_monitoring.t_base_visits tbv
+	    JOIN utilisateurs.t_roles r
+	    ON r.id_role = tbv.id_digitiser
+	    GROUP BY id_base_visit, r.nom_role, prenom_role
+	)
 /*cor_deter_obsvation as (	
  SELECT obscompl.id_observation as id_observation,
  --(obscompl.data -> 'determinateur')::json as deterjson,
@@ -76,11 +88,11 @@ SELECT o.uuid_observation AS unique_id_sinp,
 	source.id_source,
 	o.id_observation AS entity_source_pk_value,
 	v.id_dataset,
-	ref_nomenclatures.get_id_nomenclature('NAT_OBJ_GEO', 'St') AS id_nomenclature_geo_object_nature,
+	-- ref_nomenclatures.get_id_nomenclature('NAT_OBJ_GEO', 'St') AS id_nomenclature_geo_object_nature,
 	--grp_method,
-	v.id_nomenclature_grp_typ,
+	-- v.id_nomenclature_grp_typ,
 	-- TYP_GRP
-	v.id_nomenclature_tech_collect_campanule,
+	-- v.id_nomenclature_tech_collect_campanule,
 	--TECHNIQUE_OBS
 	-- id_nomenclature_bio_status, -- STATUT_BIO
 	(
@@ -173,7 +185,8 @@ SELECT o.uuid_observation AS unique_id_sinp,
 	v.date_max,
 	--validator
 	--validation_comment
-	obs.observers,
+	v.observers_txt as observers,
+	det.determiner as determiner,
 	--determinateurs_monit.determinateurs AS determiner,
 	v.id_digitiser,
 	--id_nomenclature_determination_method
@@ -196,6 +209,7 @@ FROM gn_monitoring.t_observations o
 	JOIN taxonomie.taxref t ON t.cd_nom = o.cd_nom
 	JOIN source ON TRUE
 	JOIN observers obs ON obs.id_base_visit = v.id_base_visit
+	JOIN determiner det ON det.id_base_visit = v.id_base_visit
 	LEFT JOIN LATERAL ref_geo.fct_get_altitude_intersection(s.geom_local) alt (altitude_min, altitude_max) ON TRUE --left JOIN determinateurs_monit on determinateurs_monit.id_observation = o.id_observation
 WHERE m.module_code = :'module_code';
 SELECT *
